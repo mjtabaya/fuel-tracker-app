57832e96d01b8d592811e5d5613e90656d67cf91
mjtabaya <mjtabaya@gmail.com>
2019-08-29 17:54:08 +0800

    Add name fields and role to User

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,9 @@
 class RegistrationsController < ApplicationController
   def create
     user = User.create!(
+      first_name: params['user']['first_name'],
+      last_name: params['user']['last_name'],
+      role: params['user']['role'],
       email: params['user']['email'],
       password: params['user']['password'],
       password_confirmation: params['user']['password_confirmation']
--- db/migrate/20190829041145_create_users.rb
+++ db/migrate/20190829041145_create_users.rb
@@ -3,6 +3,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :role
       t.string :email
       t.string :password_digest
 
