6bbaf6eeb0f4e7473bacba99f6cf60ad1e400b12
mjtabaya <mjtabaya@gmail.com>
2019-08-29 20:06:53 +0800

    Rails generate RefuellingHistory

---
+++ app/models/refuelling_history.rb
@@ -0,0 +1,3 @@
+class RefuellingHistory < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20190829120118_create_refuelling_histories.rb
@@ -0,0 +1,15 @@
+class CreateRefuellingHistories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :refuelling_histories do |t|
+      t.timestamp :date_refuelled
+      t.string :driver
+      t.string :vehicle
+      t.integer :odometer_reading
+      t.string :refuel_location
+      t.decimal :liters_of_fuel
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
