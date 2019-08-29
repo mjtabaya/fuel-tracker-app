8f70c2fdf19fb23e1c346e991eee1a057875e9fe
mjtabaya <mjtabaya@gmail.com>
2019-08-29 19:59:41 +0800

    Update User spec for additional fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,18 +6,26 @@ require 'spec_helper'
 RSpec.describe User, type: :model do
   context 'validations' do
     it 'should return errors' do
-      user = User.new(email: '', password: '', role: '')
+      user = User.new(
+        first_name: '',
+        last_name: '',
+        role: '',
+        email: '',
+        password: ''
+      )
 
       user.valid?
-      expect(user.errors.size).to eq 3
+      expect(user.errors.size).to eq 5
     end
 
     it 'should pass' do
       user = User.new(
-        email: 'admin@admin.com',
+        first_name: 'unangalan',
+        last_name: 'hulingalan',
+        role: 'manager',
+        email: 'manager@manager.com',
         password: 'asdfasdf',
-        password_confirmation: 'asdfasdf',
-        role: 'manager'
+        password_confirmation: 'asdfasdf'
       )
 
       expect(user.valid?).to eq true
