6e885ce64a265c4cc0a74719dd51e7d12c70ed7c
mjtabaya <mjtabaya@gmail.com>
2019-08-30 20:31:44 +0800

    Add specs for login and logout

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -20,4 +20,42 @@ RSpec.describe SessionsController, type: :request do
       expect(parsed_body['status']).to be == 'created'
     end
   end
+
+  context 'login and logout' do
+    before(:each) do
+      User.new(
+        first_name: 'Alpha',
+        last_name: 'Anre',
+        role: 'manager',
+        email: 'alpha@gmail.com',
+        password: 'asdfasdf',
+        password_confirmation: 'asdfasdf'
+      ).save
+    end
+
+    it 'returns logged in' do
+      user = {
+        email: 'alpha@gmail.com',
+        password: 'asdfasdf'
+      }
+      post '/sessions', params: { user: user }
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['logged_in']).to be == true
+    end
+
+    it 'returns logged out' do
+      user = {
+        first_name: 'Beta',
+        last_name: 'Sorn',
+        role: 'manager',
+        email: 'beta@gmail.com',
+        password: 'asdfasdf',
+        password_confirmation: 'asdfasdf'
+      }
+      post '/registrations', params: { user: user }
+      delete '/logout'
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['logged_out']).to be == true
+    end
+  end
 end
