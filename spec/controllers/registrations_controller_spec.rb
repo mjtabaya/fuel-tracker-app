05695ef2af0ba7334e838e57db6724d4d1984f04
mjtabaya <mjtabaya@gmail.com>
2019-09-02 10:42:46 +0800

    Put context in proper place

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
-require 'registrations_histories_controller'
+require 'registrations_controller'
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :request do
   context 'register new user' do
-    it 'returns a created response'
+    it 'returns a created response' do
+      user = {
+        first_name: 'Alpha',
+        last_name: 'Anre',
+        role: 'manager',
+        email: 'alpha@gmail.com',
+        password: 'asdfasdf',
+        password_confirmation: 'asdfasdf'
+      }
+      post '/registrations', params: { user: user }
+      parsed_body = JSON.parse(response.body)
+      pp parsed_body['status']
+      expect(parsed_body['status']).to be == 'created'
+    end
   end
-
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,23 +4,6 @@ require 'sessions_controller'
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :request do
-  context 'POST #create' do
-    it 'returns a created response' do
-      user = {
-        first_name: 'Alpha',
-        last_name: 'Anre',
-        role: 'manager',
-        email: 'alpha@gmail.com',
-        password: 'asdfasdf',
-        password_confirmation: 'asdfasdf'
-      }
-      post '/registrations', params: { user: user }
-      parsed_body = JSON.parse(response.body)
-      pp parsed_body['status']
-      expect(parsed_body['status']).to be == 'created'
-    end
-  end
-
   context 'login and logout' do
     before(:each) do
       User.new(
