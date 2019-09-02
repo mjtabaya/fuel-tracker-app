9b0cb2c55f6d8ab195af5c1f278e4d80ef86a504
mjtabaya <mjtabaya@gmail.com>
2019-09-02 13:16:05 +0800

    Refactor to use factory bot

--- spec/controllers/refuelling_histories_controller_spec.rb
+++ spec/controllers/refuelling_histories_controller_spec.rb
@@ -4,34 +4,11 @@ require 'refuelling_histories_controller'
 require 'rails_helper'
 
 RSpec.describe RefuellingHistoriesController, type: :request do
-  context 'get/refuelling_histories and create' do
-
+  context 'get/refuelling_histories' do
     before(:each) do
-      User.new(
-        first_name: 'admin',
-        last_name: 'admin',
-        role: 'manager',
-        email: 'admin@gmail.com',
-        password: 'asdfasdf',
-        password_confirmation: 'asdfasdf'
-      ).save
-      user = User.first
-      user.refuelling_histories.new(
-        date_refuelled: DateTime.now,
-        driver: 'testdriver',
-        vehicle: 'testvehicle',
-        odometer_reading: 2222,
-        refuel_location: 'testlocation',
-        liters_of_fuel: 3333
-      ).save
-      user.refuelling_histories.new(
-        date_refuelled: DateTime.now,
-        driver: 'testdriver1',
-        vehicle: 'testvehicle1',
-        odometer_reading: 3333,
-        refuel_location: 'testlocation1',
-        liters_of_fuel: 4444
-      ).save
+      user = create(:user)
+      user.refuelling_histories.new(attributes_for(:refuelling_history)).save
+      user.refuelling_histories.new(attributes_for(:refuelling_history)).save
     end
 
     it 'returns all refuelling histories' do
@@ -48,18 +25,13 @@ RSpec.describe RefuellingHistoriesController, type: :request do
       expect(parsed_body).to be == RefuellingHistory.first.attributes
       .except('created_at', 'date_refuelled', 'updated_at', 'liters_of_fuel')
     end
+  end
 
+  context 'create new refuelling_history' do
     it 'returns a created response' do
-      user = User.first
-      refuelling_history = {
-        date_refuelled: DateTime.now,
-        driver: 'testdriver2',
-        vehicle: 'testvehicle2',
-        odometer_reading: 4444,
-        refuel_location: 'testlocation2',
-        liters_of_fuel: 5555
-      }
-      post '/refuelling_histories', params: {id: user.id, refuelling_history: refuelling_history}
+      user = create(:user)
+      post '/refuelling_histories',
+            params: {id: user.id, refuelling_history: attributes_for(:refuelling_history)}
       parsed_body = JSON.parse(response.body)
       expect(parsed_body['status']).to be == 'created'
     end
