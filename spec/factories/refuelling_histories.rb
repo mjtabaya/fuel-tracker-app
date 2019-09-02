bb43e47f801482c63795ddc3be59b3fa2ac587c4
mjtabaya <mjtabaya@gmail.com>
2019-09-02 13:06:49 +0800

    Refactor to use factory bot

---
+++ spec/factories/refuelling_histories.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :refuelling_history do
+    date_refuelled {Time.now}
+    driver {'Manong'}
+    vehicle {'red sports car toyota'}
+    odometer_reading {200}
+    refuel_location {'Mercedes Ave., Corner Market Ave.'}
+    liters_of_fuel {5}
+  end
+end
--- spec/models/refuelling_history_spec.rb
+++ spec/models/refuelling_history_spec.rb
@@ -8,14 +8,7 @@ RSpec.describe RefuellingHistory, type: :model do
 
   context 'validations' do
     before(:each) do
-      User.new(
-        first_name: 'Alpha',
-        last_name: 'Anre',
-        role: 'manager',
-        email: 'alpha@gmail.com',
-        password: 'asdfasdf',
-        password_confirmation: 'asdfasdf'
-      ).save
+      create(:user)
     end
 
     it 'should return errors' do
@@ -34,15 +27,7 @@ RSpec.describe RefuellingHistory, type: :model do
     end
 
     it 'should pass' do
-      history = User.first.refuelling_histories.new(
-        date_refuelled: Time.now,
-        driver: 'Manong',
-        vehicle: 'SUV',
-        odometer_reading: '200',
-        refuel_location: 'Mercedes Ave., Corner Market Ave.',
-        liters_of_fuel: '10'
-      )
-
+      history = User.first.refuelling_histories.new(attributes_for(:refuelling_history))
       expect(history.valid?).to eq true
     end
   end
